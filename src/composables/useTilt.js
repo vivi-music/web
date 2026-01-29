fc089abc4c617517e4846e371167845e234b8054
Tobias <37149164+T-Boyke@users.noreply.github.com>
2026-01-29 12:04:07 +0100

    Update useTilt.js

--- src/composables/useTilt.js
+++ src/composables/useTilt.js
@@ -1,1 +1,13 @@
+import { onMounted, onUnmounted } from 'vue'
 
+export function useTilt(elementRef) {
+  const handleMove = (e) => {
+    if (!elementRef.value) return
+    const x = (window.innerWidth / 2 - e.pageX) / 30
+    const y = (window.innerHeight / 2 - e.pageY) / 30
+    elementRef.value.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`
+  }
+
+  onMounted(() => window.addEventListener('mousemove', handleMove))
+  onUnmounted(() => window.removeEventListener('mousemove', handleMove))
+}
