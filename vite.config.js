d309189f0f8779e78487264cd9d82c4c5728ab87
Tobias <37149164+T-Boyke@users.noreply.github.com>
2026-01-29 14:27:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
-// TODO: Replace with your actual repository name
-// If deploying to [https://org.github.io/repo-name/](https://org.github.io/repo-name/), set base to '/repo-name/'
-// If deploying to [https://org.github.io/](https://org.github.io/), set base to '/'
+// WICHTIG: Ändere dies von '/webpage/' zu '/web/'
+// (Da dein Repository "web" heißt)
 const repoName = '/web/' 
 
 export default defineConfig({
