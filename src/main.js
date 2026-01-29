fc72aedfb3f5559e9b41f146565088a86b78617f
Tobias <37149164+T-Boyke@users.noreply.github.com>
2026-01-29 15:52:44 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import './assets/css/base.css'
 import 'virtual:uno.css'
 import App from './App.vue'
-import i18n from './i18n' // <--- Importieren
+import i18n from './i18n' // <--- Neu: Import
 
 const app = createApp(App)
 
-app.use(i18n) // <--- Plugin nutzen
+app.use(i18n) // <--- Neu: Plugin nutzen
 app.mount('#app')
